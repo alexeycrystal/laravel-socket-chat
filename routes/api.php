3ec5e9cb5111e6529e849ecfdf024eb7b7b7dfbb
shabramov <alex.shabramov@rankactive.com>
2020-06-20 15:15:46 +0300

    Addition - Cors - additions;

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 if(!headers_sent()) {
 
-    header("Access-Control-Allow-Origin: *");
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
     header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With, GoogleDataStudio');
     header('Access-Control-Allow-Credentials: true');
